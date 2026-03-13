1364c1cdf78691d973cf3edf199f947603f5cca5
Biswajeet kumar <biswajeetk497@gmail.com>
2026-03-14 00:19:25 +0530

    fix: download ML model from huggingface during startup

--- src/services/performance_analyzer.py
+++ src/services/performance_analyzer.py
@@ -4,13 +4,17 @@ import pandas as pd
 import requests
 
 MODEL_PATH = "src/models/performance_model.pkl"
+
 MODEL_URL = "https://huggingface.co/Biswajeet1/study_forge/resolve/main/performance_model.pkl"
 
-# Download model if not present
+
+# download model if not present
 if not os.path.exists(MODEL_PATH):
+
     os.makedirs("src/models", exist_ok=True)
 
     print("Downloading ML model...")
+
     r = requests.get(MODEL_URL)
 
     with open(MODEL_PATH, "wb") as f:
@@ -19,10 +23,10 @@ if not os.path.exists(MODEL_PATH):
 model = joblib.load(MODEL_PATH)
 
 
-def predict_performance(hours_studied, previous_scores, sleep_hours, sample_papers_practiced):
+def predict_performance(hours, score, sleep, papers):
 
     input_data = pd.DataFrame(
-        [[hours_studied, previous_scores, sleep_hours, sample_papers_practiced]],
+        [[hours, score, sleep, papers]],
         columns=[
             "Hours Studied",
             "Previous Scores",
