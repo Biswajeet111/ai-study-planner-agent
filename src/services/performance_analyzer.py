4353e6d3013291588eb64b5fd4c9f30b3cbcea09
Biswajeet kumar <biswajeetk497@gmail.com>
2026-03-14 00:11:16 +0530

    feat: load ML model from HuggingFace for deployment

--- src/services/performance_analyzer.py
+++ src/services/performance_analyzer.py
@@ -1,12 +1,26 @@
+import os
 import joblib
 import pandas as pd
+import requests
 
 MODEL_PATH = "src/models/performance_model.pkl"
+MODEL_URL = "https://huggingface.co/Biswajeet1/study_forge/resolve/main/performance_model.pkl"
+
+# Download model if not present
+if not os.path.exists(MODEL_PATH):
+    os.makedirs("src/models", exist_ok=True)
+
+    print("Downloading ML model...")
+    r = requests.get(MODEL_URL)
+
+    with open(MODEL_PATH, "wb") as f:
+        f.write(r.content)
 
 model = joblib.load(MODEL_PATH)
 
 
 def predict_performance(hours_studied, previous_scores, sleep_hours, sample_papers_practiced):
+
     input_data = pd.DataFrame(
         [[hours_studied, previous_scores, sleep_hours, sample_papers_practiced]],
         columns=[
@@ -18,4 +32,5 @@ def predict_performance(hours_studied, previous_scores, sleep_hours, sample_pape
     )
 
     prediction = model.predict(input_data)
+
     return round(float(prediction[0]), 2)
